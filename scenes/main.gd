51755340b393f1b955e5a579248636f75b823000
Ian <ian.cantor@fandm.edu>
2018-08-10 21:49:25 -0400

    Adds action scene with button textures and game state switching

--- scenes/main.gd
+++ scenes/main.gd
@@ -1,5 +1,9 @@
 extends Node
 
+enum gameState { PROMPT, ACTION, RESULT }
+
+var actionsMenu
+var currentState
 var textIsDone = false
 var textLabel
 var textQueue = []
@@ -7,19 +11,28 @@ var textQueueIndex = 0
 var textTimer
 
 func _ready():
+	actionsMenu = $actionsMenu/actions
+	currentState = PROMPT
 	textLabel = $ui/textbox/label
 	textTimer = $ui/textbox/textTimer
 
-	prepare_text_queue(["This is a test...", "Does it work?"], 0)
+	prepare_text_queue(["The creature is newly born.", "What do you do?"], 0)
 
 	set_process_input(true)
 
 func _input(event):
 	if event.is_action_pressed("ui_accept"):
 		if textIsDone:
-			pass
+			changeState()
 		elif textLabel.get_visible_characters() >= textLabel.get_total_character_count():
 			prepare_text_queue(textQueue, textQueueIndex)
+
+func changeState():
+	if currentState == gameState.PROMPT:
+		currentState = gameState.ACTION
+		actionsMenu.show()
+	elif currentState == gameState.ACTION:
+		currentState = gameState.RESULT
 
 func prepare_text_queue(textArray, textIndex):
 	textQueue = textArray
