6be37d93d70091a4fcdb1c50499615bdbaf8d9c2
Ian <ian.cantor@fandm.edu>
2018-08-11 11:10:46 -0400

    Implements anger death, relogs pet data

--- globals/pet.gd
+++ globals/pet.gd
@@ -24,7 +24,10 @@ func play():
 func scold():
 	pet.happiness -= 3
 	pet.hunger -= 1
-	pet.size -= 1
+	if pet.size -1 <= 0:
+		pet.size = 0
+	else:
+		pet.size -= 1
 	result()
 
 func release():
@@ -34,17 +37,13 @@ func result():
 	var textQueue = []
 	pet.age += 1
 
-	if pet.size <= 2:
-		textQueue.append("It's a healthy size...")
-		textQueue.append("...for now.")
-	elif pet.size == 3:
-		textQueue.append("It's growing large...")
-	elif pet.size == 4:
-		textQueue.append("It barely fits in here!")
-	elif pet.size == 5:
-		textQueue.append("There's no room for it to fit!")
-		textQueue.append("Quick, slim it down!")
-	elif pet.size > 5:
+	if pet.happiness <= 0:
+		textQueue.append("It's got so angry...")
+		textQueue.append("...that it exploded!")
+		textQueue.append("How sad. You monster.")
+		isDead = true
+
+	if pet.size > 5:
 		textQueue.append("You've overfed the creature.")
 		textQueue.append("You ran out of space to fit it...")
 		textQueue.append("...and it exploded.")
@@ -52,6 +51,23 @@ func result():
 		isDead = true
 
 	if !isDead:
+		if pet.size <= 2:
+			textQueue.append("It's a healthy size...")
+			textQueue.append("...for now.")
+		elif pet.size == 3:
+			textQueue.append("It's growing large...")
+		elif pet.size == 4:
+			textQueue.append("It barely fits in here!")
+		elif pet.size == 5:
+			textQueue.append("There's no room for it to fit!")
+			textQueue.append("Quick, slim it down!")
+		elif pet.size > 5:
+			textQueue.append("You've overfed the creature.")
+			textQueue.append("You ran out of space to fit it...")
+			textQueue.append("...and it exploded.")
+			textQueue.append("I hope you're happy...")
+			isDead = true
+
 		if pet.happiness < 5:
 			textQueue.append("It's angry!")
 		elif pet.happiness >= 5:
@@ -65,4 +81,5 @@ func result():
 			textQueue.append("What will you do?")
 
 	gameState.currentState = gameState.RESULT
+	print(pet)
 	return textQueue
