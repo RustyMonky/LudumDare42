89ba90479498bbe4930ac8a42a2a6eea864b4187
Ian <ian.cantor@fandm.edu>
2018-08-12 15:39:20 -0400

    Adds explosion particles for pink sprite death

--- scenes/pet.gd
+++ scenes/pet.gd
@@ -1,10 +1,12 @@
 extends Sprite
 
 var animationPlayer
+var particles
 var spriteTexture
 
 func _ready():
 	animationPlayer = $animation
+	particles = $particles
 
 func explode():
 	animationPlayer.play("explode")
