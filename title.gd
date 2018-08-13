51fcdeb541c32745dd82fb04fabc55a68ffd27dd
Ian <ian.cantor@fandm.edu>
2018-08-13 10:13:45 -0400

    Adds main sound loop to game, ups title fade out time

--- title.gd
+++ title.gd
@@ -10,8 +10,8 @@ func _ready():
 	titleTween = $titleTween
 
 func _on_start_pressed():
-	titleTween.interpolate_property(background, "modulate", Color(1,1,1,1), Color(0,0,0,1), 1, Tween.TRANS_LINEAR, Tween.EASE_OUT)
-	titleTween.interpolate_property(titleMusic, "volume_db", -10, -20, 1, Tween.TRANS_LINEAR, Tween.EASE_OUT)
+	titleTween.interpolate_property(background, "modulate", Color(1,1,1,1), Color(0,0,0,1), 1.5, Tween.TRANS_LINEAR, Tween.EASE_OUT)
+	titleTween.interpolate_property(titleMusic, "volume_db", -10, -20, 1.5, Tween.TRANS_LINEAR, Tween.EASE_OUT)
 	titleTween.start()
 
 func _on_titleTween_tween_completed(object, key):
